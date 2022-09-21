d8b2a109c95f25c6e4531e39cccef4142fc50db4
Dilsha Thathsarani <86911572+Dilsha-thathsarani@users.noreply.github.com>
2022-09-22 00:23:46 +0530

    Fully complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,147 +1,145 @@
-import React, { useState } from 'react'
-import PayNow from './PayNow'
-import './Style/card.css'
+import React, { useState } from "react";
+import PayNow from "./PayNow";
+import "./Style/card.css";
 
-export default function Card (props) {
-  const { projects } = props
-  const [darkMode, setDarkMode] = useState(false)
-  const [clickBtnId, setClickBtnId] = useState('')
-  const [paymentComplete, setPaymentComplete] = useState(false)
-  const [donateAmount, setDonateAmount] = useState('')
+export default function Card(props) {
+  const { projects } = props;
+  const [darkMode, setDarkMode] = useState(false);
+  const [clickBtnId, setClickBtnId] = useState("");
+  const [paymentComplete, setPaymentComplete] = useState(false);
+  const [donateAmount, setDonateAmount] = useState("");
 
-  const handleDonate = id => {
-    setClickBtnId(id)
-  }
+  const handleDonate = (id) => {
+    setClickBtnId(id);
+  };
 
   return (
-    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
-      <div className='container'>
-        <div className='top-header'>
-          <div className='logo'></div>
-          <div className='toggle-btn'>
-            <span className='toggle-icon'>
-              {!darkMode ? <div className='sun' /> : <div className='moon' />}
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <div className="top-header">
+          <div className="logo"></div>
+          <div className="toggle-btn">
+            <span className="toggle-icon">
+              {!darkMode ? <div className="sun" /> : <div className="moon" />}
             </span>
-            <label className='switch'>
+            <label className="switch">
               <input
-                type='checkbox'
+                type="checkbox"
                 checked={darkMode}
                 onChange={() => setDarkMode(!darkMode)}
               />
-              <span className='slider round'></span>
+              <span className="slider round"></span>
             </label>
           </div>
         </div>
 
         {/* header */}
-        <div className='header-banner'>
-          <div className='header-image' />
-          <div className='header-text'>
+        <div className="header-banner">
+          <div className="header-image" />
+          <div className="header-text">
             <h1>We want give them a better tommorow!</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s
             </p>
-            <button className='header-btn'>Get Started</button>
+            <button className="header-btn">Get Started</button>
           </div>
         </div>
 
         {/* title */}
-        <div className='title-section'>
-          <div className='title-logo' />
-          <div className='title-text'>Projects</div>
+        <div className="title-section">
+          <div className="title-logo" />
+          <div className="title-text">Projects</div>
         </div>
 
         {/* cards set */}
-        <div className='cards-container'>
+        <div className="cards-container">
           {projects?.map((project, index) => (
-            <div className='card' key={index}>
-              <div className='card'>
-                {clickBtnId === project.id ? (
-                  <>
-                    {paymentComplete ? (
-                      <div className=''>
-                        <div className='successful'></div>
-                        <div className='donate'>
-                          You have successfully made the donation!
-                        </div>
+            <div className="card" key={index}>
+              {clickBtnId === project.id ? (
+                <>
+                  {paymentComplete ? (
+                    <div className="">
+                      <div className="successful"></div>
+                      <div className="donate">
+                        You have successfully made the donation!
                       </div>
-                    ) : (
-                      <PayNow
-                        setClickBtnId={setClickBtnId}
-                        donateAmount={donateAmount}
-                        setDonateAmount={setDonateAmount}
-                      />
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <div className='card-image'>
-                      <img src={project.urls.small} alt='random' />
                     </div>
-                    <span className='card-title'>{project.title}</span>
-                    <input
-                      type='range'
-                      min='0'
-                      max='100000'
-                      className={`range ${
-                        project.target_amount >= 30000 &&
-                        project.target_amount < 44999
-                          ? 'orange-bg'
-                          : project.target_amount > 45000
-                          ? 'green-bg'
-                          : ''
-                      }`}
-                      name='rangeInput'
-                      value={project.target_amount}
+                  ) : (
+                    <PayNow
+                      setClickBtnId={setClickBtnId}
+                      donateAmount={donateAmount}
+                      setDonateAmount={setDonateAmount}
                     />
-                    <div className='price'>
-                      <span>$ {project.target_amount}</span>
-                      <span className='secondary-text'> Raised</span>
-                    </div>
-                  </>
-                )}
+                  )}
+                </>
+              ) : (
+                <>
+                  <div className="card-image">
+                    <img src={project.urls.small} alt="random" />
+                  </div>
+                  <span className="card-title">{project.title}</span>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100000"
+                    className={`range ${
+                      project.target_amount >= 30000 &&
+                      project.target_amount < 44999
+                        ? "orange-bg"
+                        : project.target_amount > 45000
+                        ? "green-bg"
+                        : ""
+                    }`}
+                    name="rangeInput"
+                    value={project.target_amount}
+                  />
+                  <div className="price">
+                    <span>$ {project.target_amount}</span>
+                    <span className="secondary-text"> Raised</span>
+                  </div>
+                </>
+              )}
 
-                <div className='button-wrap'>
-                  <button
-                    className='donate-now'
-                    disabled={clickBtnId === project.id && donateAmount === ''}
-                    onClick={() => {
-                      if (clickBtnId !== project.id) {
-                        handleDonate(project.id)
+              <div className="button-wrap">
+                <button
+                  className="donate-now"
+                  disabled={clickBtnId === project.id && donateAmount === ""}
+                  onClick={() => {
+                    if (clickBtnId !== project.id) {
+                      handleDonate(project.id);
+                    } else {
+                      if (paymentComplete) {
+                        setPaymentComplete(false);
+                        setClickBtnId("");
+                        setDonateAmount("");
                       } else {
-                        if (paymentComplete) {
-                          setPaymentComplete(false)
-                          setClickBtnId('')
-                          setDonateAmount('')
-                        } else {
-                          setPaymentComplete(true)
-                        }
+                        setPaymentComplete(true);
                       }
-                    }}
-                    style={{
-                      background:
-                        clickBtnId === project.id
-                          ? paymentComplete
-                            ? '#AF93FF'
-                            : '#F8C16F'
-                          : ''
-                    }}
-                  >
-                    {clickBtnId === project.id ? (
-                      <>{paymentComplete ? 'Done' : 'Pay Now'}</>
-                    ) : (
-                      'Donate Now'
-                    )}
-                  </button>
-                </div>
+                    }
+                  }}
+                  style={{
+                    background:
+                      clickBtnId === project.id
+                        ? paymentComplete
+                          ? "#AF93FF"
+                          : "#F8C16F"
+                        : "",
+                  }}
+                >
+                  {clickBtnId === project.id ? (
+                    <>{paymentComplete ? "Done" : "Pay Now"}</>
+                  ) : (
+                    "Donate Now"
+                  )}
+                </button>
               </div>
             </div>
           ))}
-          <button className='load-btn'>More Projects</button>
+          <button className="load-btn">More Projects</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/PayNow.js
+++ src/components/PayNow.js
@@ -1,71 +1,71 @@
-import React from 'react'
-import './Style/card.css'
+import React from "react";
+import "./Style/card.css";
 
-export default function PayNow ({
+export default function PayNow({
   setClickBtnId,
   donateAmount,
-  setDonateAmount
+  setDonateAmount,
 }) {
-  const handleChange = e => {
-    setDonateAmount(e.target.value)
-  }
+  const handleChange = (e) => {
+    setDonateAmount(e.target.value);
+  };
   return (
     <div>
       <div
         onClick={() => {
-          setClickBtnId('')
-          setDonateAmount('')
+          setClickBtnId("");
+          setDonateAmount("");
         }}
-        className='close'
+        className="close"
       >
         &times;
       </div>
-      <h2 className='pay-title'>Select the amount that you want to donate</h2>
-      <div className='radio-btn'>
+      <h2 className="pay-title">Select the amount that you want to donate</h2>
+      <div className="radio-btn">
         <input
-          type='radio'
-          value='10'
-          checked={donateAmount === '10'}
-          onChange={e => handleChange(e)}
+          type="radio"
+          value="10"
+          checked={donateAmount === "10"}
+          onChange={(e) => handleChange(e)}
         />
-          <label for='html'>$10</label>
+          <label for="html">$10</label>
         <br></br>
       </div>
-      <div className='radio-btn'>
+      <div className="radio-btn">
         <input
-          type='radio'
-          value='50'
-          checked={donateAmount === '50'}
-          onChange={e => handleChange(e)}
+          type="radio"
+          value="50"
+          checked={donateAmount === "50"}
+          onChange={(e) => handleChange(e)}
         />
-          <label for='html'>$50</label>
+          <label for="html">$50</label>
         <br></br>
       </div>
-      <div className='radio-btn'>
+      <div className="radio-btn">
         <input
-          type='radio'
-          value='100'
-          checked={donateAmount === '100'}
-          onChange={e => handleChange(e)}
+          type="radio"
+          value="100"
+          checked={donateAmount === "100"}
+          onChange={(e) => handleChange(e)}
         />
-          <label for='html'>$100</label>
+          <label for="html">$100</label>
         <br></br>
       </div>
-      <div className='radio-btn'>
+      <div className="radio-btn">
         <input
-          type='radio'
-          value='500'
-          checked={donateAmount === '500'}
-          onChange={e => handleChange(e)}
+          type="radio"
+          value="500"
+          checked={donateAmount === "500"}
+          onChange={(e) => handleChange(e)}
         />
-          <label for='html'>$500</label>
+          <label for="html">$500</label>
         <br></br>
       </div>
-      <div className='radio-btn'>
-        <input type='radio' id='html' name='fav_language' value='1000' />
-          <label for='html'>$1000</label>
+      <div className="radio-btn">
+        <input type="radio" id="html" name="fav_language" value="1000" /> {" "}
+        <label for="html">$1000</label>
         <br></br>
       </div>
     </div>
-  )
+  );
 }
